041de2e3ec2ffaf40e60e915f37e603085d75740
Wences <villegaswences@gmail.com>
2020-09-13 23:48:38 -0300

    Activado loading del model

--- server.py
+++ server.py
@@ -106,5 +106,5 @@ def predict():
 # cuando corramos python esta_script.py detecta que el hilo de ejecucion primario es este (main), carga el modelo y luego el server
 if __name__ == "__main__":
 	print("Cargando el modelo e iniciando el servidor Flask!")
-	# load_model()
+	load_model()
 	app.run(host='0.0.0.0')
